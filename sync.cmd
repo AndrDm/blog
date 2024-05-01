11707c42c4b3edcc417d92c02c7b32263da3a0ac
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2024-05-01 09:35:36 +0200

    changes

---
+++ sync.cmd
@@ -0,0 +1,7 @@
+cd "C:\Users\Andrey\Desktop\blog"
+echo n | C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe pull
+C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe add .
+echo n | C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe pull
+C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe commit -am "changes"
+C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe push
+C:\Users\Andrey\AppData\Local\GitHubDesktop\app-3.3.13\resources\app\git\mingw64\bin\git.exe count-objects -vH
