0c90efff6655daaed8df4105d019b96e278671a2
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2024-12-21 09:07:46 +0100

    cleanup

--- drafts/PLC Article/X20CP1586AS4SHA256rdtsc/Temp/Includes/CycleTime/AsDefault.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CYCLETIME_DEFAULT_1614107143
-#define _CYCLETIME_DEFAULT_1614107143
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <typesTYP.h>
-#include <variablesVAR.h>
-#endif
