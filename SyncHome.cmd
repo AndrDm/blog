2f3e84d0743680abb2fc392e054d709696cc3da2
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2025-01-07 08:00:42 +0100

    changes

---
+++ SyncHome.cmd
@@ -0,0 +1,19 @@
+cd "C:\Users\Andrey\Desktop\blog"
+
+"C:\Program Files\Git\mingw64\bin\git.exe" config --local core.autocrlf false
+
+"C:\Program Files\Git\mingw64\bin\git.exe" pull
+
+"C:\Program Files\Git\mingw64\bin\git.exe" push
+
+"C:\Program Files\Git\mingw64\bin\git.exe" add .
+
+"C:\Program Files\Git\mingw64\bin\git.exe" pull
+
+"C:\Program Files\Git\mingw64\bin\git.exe" commit -am "changes"
+
+"C:\Program Files\Git\mingw64\bin\git.exe" push
+
+"C:\Program Files\Git\mingw64\bin\git.exe" count-objects -vH
+
+"C:\Program Files\Git\mingw64\bin\git.exe" pull
